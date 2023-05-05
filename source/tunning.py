366ed6dce1b30ccd9befae4c12352e5902e10d8a
Matheus Henrique de Cerqueira Pinto <cerqra@icex.head.ice.cemeai.icmc.usp.br>
2023-05-05 14:45:23 -0300

    Update source

--- source/tunning.py
+++ source/tunning.py
@@ -1,6 +1,8 @@
 # Import libraries
 
-import nltk
+import os
+import sys
+from tqdm import tqdm
 from octis.models.CTM import CTM
 from octis.models.ETM import ETM
 from octis.models.HDP import HDP
@@ -9,6 +11,7 @@ from octis.models.LSI import LSI
 from octis.models.NMF import NMF
 from octis.models.ProdLDA import ProdLDA
 from octis.dataset.dataset import Dataset
+from useful_fun import HiddenPrints
 from octis.models.NeuralLDA import NeuralLDA
 from octis.optimization.optimizer import Optimizer
 from skopt.space.space import Real, Categorical, Integer
@@ -18,41 +21,47 @@ from octis.evaluation_metrics.diversity_metrics import TopicDiversity
 from custom.metrics.TDCI import TDCI
 # from custom_models import CustomTop2Vec
 
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
 base_csv_path = "tunning/csv/"
 base_res_path = "tunning/res/"
+dataset_folder_path = ""
+dataset_name = ""
 
 def __optimize(model: any, search_space: dict, save_path: str, csv_path: str):
     """
     Optimize a given model in a search space.
 
     """
-
     # Create optimizer
     optimizer = Optimizer()
 
-    # Optimize
-    ctm_optimization_result = optimizer.optimize(
-        model,
-        dataset,
-        tdci,
-        search_space,
-        random_state=42,
-        model_runs=model_runs,
-        save_models=True,
-        # to keep track of other metrics
-        extra_metrics=[topic_coherence, topic_diversity],
-        number_of_call=optimization_runs,
-        save_path=save_path,
-    )
+    with HiddenPrints():
+        # Optimize
+        optimization_result = optimizer.optimize(
+            model,
+            dataset,
+            tdci,
+            search_space,
+            random_state=42,
+            model_runs=model_runs,
+            save_models=True,
+            # to keep track of other metrics
+            extra_metrics=[topic_coherence, topic_diversity],
+            number_of_call=optimization_runs,
+            save_path=save_path,
+        )
 
-    # Export csv
-    ctm_optimization_result.save_to_csv(csv_path)
+        # Export csv
+        optimization_result.save_to_csv(csv_path)
 
 
 def optimize_ctm(n_topics: int = 10):
     # Define search space
     ctm_search_space = {
-        "n_topics": [n_topics],
+        "num_topics": [n_topics],
+        "model_type": Categorical({"LDA", "prodLDA"}),
         "num_layers": Categorical({1, 2, 3, 4}),
         "num_neurons": Categorical({10, 30, 50, 100, 200, 300}),
         "activation": Categorical(
@@ -61,7 +70,9 @@ def optimize_ctm(n_topics: int = 10):
         ),
         "solver": Categorical({"adam", "sgd"}),
         "dropout": Real(0.0, 0.95),
-        "inference_type": Categorical({"zeroshot", "combined"})
+        "inference_type": Categorical({"zeroshot", "combined"}),
+        "num_epochs": Integer(100, 500),
+        "num_samples": Integer(10, 50)
     }
 
     # Define model and optimize
@@ -69,8 +80,8 @@ def optimize_ctm(n_topics: int = 10):
     __optimize(
         model_ctm,
         ctm_search_space,
-        base_res_path + "test_ctm" + str(n_topics) + "//",
-        base_csv_path + "results_ctm" + str(n_topics) + ".csv"
+        base_res_path + "ctm" + "-" + dataset_name + "-" +str(n_topics) + "//",
+        base_csv_path + "ctm" + "-" + dataset_name + "-" +str(n_topics) + ".csv"
     )
 
 
@@ -82,81 +93,117 @@ def optimize_etm(n_topics: int = 10):
         "rho": Integer(200, 600),
         "num_neurons": Categorical({100, 200, 300}),
         "activation": Categorical({'sigmoid', 'relu', 'softplus'}),
-        "dropout": Real(0.0, 0.95)
+        "dropout": Real(0.0, 0.95),
+        "num_epochs": Integer(100, 500)
     }
 
     model_etm = ETM(device="gpu")
     __optimize(model_etm,
                etm_search_space,
-               base_res_path + "test_etm" + str(n_topics) + "//",
-               base_csv_path + "results_etm" + str(n_topics) + ".csv"
+               base_res_path + "etm" + "-" + dataset_name + "-" +str(n_topics) + "//",
+               base_csv_path + "etm" + "-" + dataset_name + "-" +str(n_topics) + ".csv"
                )
 
 
 def optimize_lda(n_topics: int = 10):
     lda_search_space = {
         "num_topics": [n_topics],
-
+        "chunksize": Integer(500, 50000),
+        "passes": Integer(1, 500),
+        "alpha": Categorical({"symmetric", "asymmetric"}),
+        "decay": Real(0.5, 1),
+        "offset": Real(0.1, 10),
+        "iterations": Integer(5, 1000),
+        "gamma_threshold": Real(0.001, 1)
     }
 
     model_lda = LDA()
     __optimize(model_lda,
                lda_search_space,
-               base_res_path + "test_lda" + str(n_topics) + "//",
-               base_csv_path + "results_lda" + str(n_topics) + ".csv"
+               base_res_path + "lda" + "-" + dataset_name + "-" +str(n_topics) + "//",
+               base_csv_path + "lda" + "-" + dataset_name + "-" +str(n_topics) + ".csv"
                )
 
 
 def optimize_lsi(n_topics: int = 10):
     lsi_search_space = {
         "num_topics": [n_topics],
-
+        "chunksize": Integer(500, 50000),
+        "decay": Real(0.5, 1),
+        # "power_iters": Integer(1, 10),
+        "extra_samples": Integer(10, 200)
     }
 
     model_lsi = LSI()
     __optimize(model_lsi,
                lsi_search_space,
-               base_res_path + "test_lsi" + str(n_topics) + "//",
-               base_csv_path + "results_lsi" + str(n_topics) + ".csv"
+               base_res_path + "lsi" + "-" + dataset_name + "-" +str(n_topics) + "//",
+               base_csv_path + "lsi" + "-" + dataset_name + "-" +str(n_topics) + ".csv"
                )
 
 
 def optimize_nmf(n_topics: int = 10):
     nmf_search_space = {
         "num_topics": [n_topics],
+        "chunksize": Integer(500, 10000),
+        "passes": Integer(1, 500),
+        "kappa": Real(0.5, 1),
+        "minimum_probability": Real(0.01, 0.1),
+        "w_max_iter": Integer(100, 500),
+        "w_stop_condition": Real(0.0001, 0.001),
+        "h_max_iter": Integer(10, 100),
+        "h_stop_condition": Real(0.001, 0.01)
     }
 
     model_nmf = NMF()
     __optimize(model_nmf,
                nmf_search_space,
-               base_res_path + "test_nmf" + str(n_topics) + "//",
-               base_csv_path + "results_nmf" + str(n_topics) + ".csv"
+               base_res_path + "nmf" + "-" + dataset_name + "-" +str(n_topics) + "//",
+               base_csv_path + "nmf" + "-" + dataset_name + "-" +str(n_topics) + ".csv"
                )
 
 
 def optimize_prodlda(n_topics: int = 10):
     prodlda_search_space = {
         "num_topics": [n_topics],
+        "activation": Categorical({"softplus", "relu"}),
+        "dropout": Real(0.0, 0.95),
+        "lr": Real(0.0001, 0.01),
+        "momentum": Real(0.5, 0.99),
+        "solver": Categorical({"adam", "sgd"}),
+        "num_epochs": Integer(50, 500),
+        "num_layers": Integer(1, 10),
+        "num_neurons": Integer(10, 1000),
+        "num_samples": Integer(5, 50)
     }
 
     model_prodlda = ProdLDA()
     __optimize(model_prodlda,
                prodlda_search_space,
-               base_res_path + "test_prodlda" + str(n_topics) + "//",
-               base_csv_path + "results_prodlda" + str(n_topics) + ".csv"
+               base_res_path + "prodlda" + "-" + dataset_name + "-" +str(n_topics) + "//",
+               base_csv_path + "prodlda" + "-" + dataset_name + "-" +str(n_topics) + ".csv"
                )
 
 
 def optimize_neurallda(n_topics: int = 10):
     neurallda_search_space = {
         "num_topics": [n_topics],
+        "activation": Categorical({"softplus", "relu"}),
+        "dropout": Real(0.0, 0.95),
+        "lr": Real(0.0001, 0.01),
+        "momentum": Real(0.5, 0.99),
+        "solver": Categorical({"adam", "sgd"}),
+        "num_epochs": Integer(50, 500),
+        "num_layers": Integer(1, 10),
+        "num_neurons": Integer(10, 1000),
+        "num_samples": Integer(5, 50)
     }
 
     model_neurallda = NeuralLDA()
     __optimize(model_neurallda,
                neurallda_search_space,
-               base_res_path + "test_neurallda" + str(n_topics) + "//",
-               base_csv_path + "results_neurallda" + str(n_topics) + ".csv"
+               base_res_path + "neurallda" + "-" + dataset_name + "-" +str(n_topics) + "//",
+               base_csv_path + "neurallda" + "-" + dataset_name + "-" +str(n_topics) + ".csv"
                )
 
 
@@ -167,8 +214,8 @@ def optimize_hdp():
     model_hdp = HDP()
     __optimize(model_hdp,
                hdp_search_space,
-               base_res_path + "test_hdp//",
-               base_csv_path + "results_hdp.csv"
+               base_res_path + "hdp//",
+               base_csv_path + "hdp.csv"
                )
 
 
@@ -180,8 +227,8 @@ def optimize_custom_top2vec():
     model_custom_top2vec = CustomTop2Vec()
     __optimize(model_custom_top2vec,
                custom_top2vec_search_space,
-               base_res_path + "test_custom_top2vec//",
-               base_csv_path + "results_custom_top2vec.csv"
+               base_res_path + "custom_top2vec//",
+               base_csv_path + "custom_top2vec.csv"
                )
 
 
@@ -193,23 +240,36 @@ def optimize_bertopic():
     model_bertopic = None
     __optimize(model_bertopic,
                bertopic_search_space,
-               base_res_path + "test_bertopic//",
-               base_csv_path + "results_bertopic.csv"
+               base_res_path + "bertopic//",
+               base_csv_path + "bertopic.csv"
                )
 
+def rm_trash():
+    if os.path.exists("_test.pkl"):
+        os.remove("_test.pkl")
+
+    if os.path.exists("_train.pkl"):
+        os.remove("_train.pkl")
+    
+    if os.path.exists("_val.pkl"):
+        os.remove("_val.pkl")
 
 if __name__ == "__main__":
-    import os
+    if (len(sys.argv) != 2):
+        print("Usage: python3 tunning.py dataset_folder_path")
+        exit(1)
 
-    # os.chdir(os.getenv("HOME"))
-    # os.chdir("octis-compair")
 
-    model_runs = 1
-    optimization_runs = 2
+    os.chdir(os.getenv("HOME"))
+    os.chdir("octis-compair")
+    dataset_folder_path = sys.argv[1]
+    dataset_name = dataset_folder_path.split("/")[-1]
+
+    model_runs = 3
+    optimization_runs = 10
 
     dataset = Dataset()
-    # dataset.load_custom_dataset_from_folder("pl_3723_2019")
-    dataset.fetch_dataset("20NewsGroup")
+    dataset.load_custom_dataset_from_folder(dataset_folder_path)
 
     # Define metric for tunning
     topic_coherence = Coherence(texts=dataset.get_corpus())
@@ -219,17 +279,21 @@ if __name__ == "__main__":
 
     tdci = TDCI(texts=dataset.get_corpus())
 
-    # Run optimizer for models depending on n_topics
-    for n_topics in [10]:
-        # optimize_ctm(n_topics)
-        optimize_etm(n_topics)
-        optimize_lda(n_topics)
-        optimize_lsi(n_topics)
-        optimize_nmf(n_topics)
-        optimize_prodlda(n_topics)
-        optimize_neurallda(n_topics)
+    optimization_topic_functions = [
+                                    optimize_ctm, 
+                                    optimize_etm,
+                                    optimize_lda, 
+                                    optimize_lsi, 
+                                    optimize_nmf, 
+                                    optimize_prodlda, 
+                                    optimize_neurallda
+                                    ]
 
-    # Models that doesn't require n_topics
-    optimize_hdp()
-    # optimize_custom_top2vec()
-    # optimize_bertopic()
+    # Run optimizer for models depending on n_topics
+    for n_topics in [10, 20, 30, 40, 50]:
+        print("Optimizing for n_topics = " + str(n_topics) + "...")
+        for func in optimization_topic_functions:
+            with HiddenPrints():
+                func(n_topics)
+                rm_trash()
+            print(str(func.__name__) + " done!")
--- source/visualizations.py
+++ source/visualizations.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -8,73 +9,31 @@ sns.set_context("paper")
 
 
 def symmetrize_y_axis(axes, y_min, y_max):
-    # y_max = np.abs(axes.get_ylim()).max()
     axes.set_ylim(ymin=y_min, ymax=y_max)
 
 
-def visualize_model_along_exec(path: str, alg_name: str):
-    # Read dataset
-    df = pd.read_csv(path)
+def visualize_model_along_exec(alg_name: str, dataset_name: str):
+    
+    dfs = []
+    n_topics = [10, 20, 30, 40, 50]
 
-    # Get model_runs
-    model_runs = df["Mean(model_runs)"]
-    topic_div = df["Coherence(not optimized)"]
+    for n_topic in n_topics:
+        dfs.append(list(pd.read_csv("tunning/csv/" + alg_name + "-" + dataset_name + "-" + str(n_topic) + ".csv")["Mean(model_runs)"]))
 
-    # Define executions
-    execs = list(range(len(model_runs)))
-
-    fig, ax = plt.subplots()
-    # fig = plt.figure()
-
-    # Define axis and title
-    plt.xlabel("Execução")
-    plt.title("Desempenho ao longo do espaço de busca - " + alg_name)
-
-    # Define topic coherence plot
-    ax1 = sns.lineplot(
-        x=execs, y=model_runs, color=sns.color_palette("tab10")[
-            0], label="TC mean"
-    )
-    ax1.set_ylabel("Topic Coherence")
-    symmetrize_y_axis(ax1, -0.6, 0.9)
-    ax1.get_legend().remove()
-
-    # Define topic diversity plot
-    plt.xlabel("Execution")
-    plt.title("Performance along search space - " + alg_name)
-    ax2 = ax1.twinx()
-    ax2 = sns.lineplot(
-        x=execs, y=topic_div, color=sns.color_palette("tab10")[
-            1], label="TD mean"
-    )
-    ax2.grid(False)
-    ax2.set_ylabel("Topic Diversity")
-    symmetrize_y_axis(ax2, 0.2, 1.0)
-    ax2.get_legend().remove()
-
-    # Legend
-    fig.legend(
-        loc="upper right", bbox_to_anchor=(0.95, 0.2), bbox_transform=ax.transAxes
-    )
-
-    # Export
-    plt.savefig("plots/" + alg_name + ".svg", format="svg", dpi=300)
-
-    # Show
-    plt.show()
-
-    # Get best
-    best_conf = (
-        df.sort_values(by="Mean(model_runs)",
-                       ascending=False).head(1).to_numpy()
-    )
-
-    # Print params
-    print("Best params")
-    for i in list(range(len(df.columns))):
-        print(df.columns[i] + " = " + str(best_conf[0][i]))
-
+    str_topics = [str(x) for x in n_topics]
+    sns.lineplot(dfs)
+    plt.xlabel("Optimization Run")
+    plt.ylabel("Mean TDCI")
+    plt.legend(str_topics)
+    plt.savefig("plots/" + dataset_name + "-" + alg_name + ".svg", format="svg", dpi=300)
+    plt.clf()
 
 if __name__ == "__main__":
-    visualize_model_along_exec("tunning_results/results_etm.csv", "ETM")
+    os.chdir(os.getenv("HOME"))
+    os.chdir("octis-compair")
+
+    for tm in ["ctm", "etm", "lda", "lsi", "nmf", "neurallda", "prodlda"]:
+        print(tm)
+        visualize_model_along_exec(tm, "pec_471_2005")
+        print("\n")
     exit(0)
